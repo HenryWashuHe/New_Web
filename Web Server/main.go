9a2601fc1ecc8ed5e5b6a9b19e92e2808c9e6494
HenryWashuHe <he.h@wustl.edu>
2025-07-26 17:44:31 +0800

    first page

--- Web Server/main.go
+++ Web Server/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello from Web Server</h1>")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Starting server on :8000")
+	http.HandleFunc("/", firstPage)
+	http.ListenAndServe(":8000", nil)
 }
